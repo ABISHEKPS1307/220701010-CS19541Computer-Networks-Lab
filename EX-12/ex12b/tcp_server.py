029b750d0891d07e915d2e8aa76c1f0a73bf0943
ABISHEK_PARTHASARATHY <abishekparthasarathy47@gmail.com>
2024-11-06 10:56:49 +0530

    Update tcp_server.py

--- EX-12/ex12b/tcp_server.py
+++ EX-12/ex12b/tcp_server.py
@@ -1,7 +1,7 @@
 import socket
 
 def start_tcp_server(host='127.0.0.1', port=65432):
-    # Create a TCP socket
+   
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(1)
