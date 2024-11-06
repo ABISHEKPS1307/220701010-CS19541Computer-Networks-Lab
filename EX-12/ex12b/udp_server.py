944e4989865fe1afc1329714ef267aa16add7d9c
ABISHEK_PARTHASARATHY <abishekparthasarathy47@gmail.com>
2024-11-06 10:57:27 +0530

    Update udp_server.py

--- EX-12/ex12b/udp_server.py
+++ EX-12/ex12b/udp_server.py
@@ -1,7 +1,7 @@
 import socket
 
 def start_udp_server(host='127.0.0.1', port=65432):
-    # Create a UDP socket
+   
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
     
